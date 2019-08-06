f39c47a17287c621c283b329b509784c905399e5
cmmadnat <cmmadnat@gmail.com>
2019-08-06 17:28:09 +0700

    add 2 more screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  createSwitchNavigator,
+  createAppContainer,
+  createStackNavigator
+} from "react-navigation";
+import { func } from "prop-types";
+import AboutScreen from "./about-screen";
 
-export default function App() {
+function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>I could be a login screen</Text>
+      <Button
+        title="Click to go to next page"
+        onPress={() => {
+          navigation.navigate("AppStack");
+        }}
+      />
     </View>
   );
 }
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>I'm a second screen</Text>
+      <Button
+        title="Click to go to third page"
+        onPress={() => {
+          navigation.navigate("About");
+        }}
+      />
+    </View>
+  );
+};
+const switchNavigator = createSwitchNavigator(
+  {
+    AuthStack: createStackNavigator({ Login: { screen: LoginScreen } }),
+    AppStack: createStackNavigator({
+      Home: { screen: HomeScreen },
+      About: { screen: AboutScreen }
+    })
+  },
+  {
+    initialRouteName: "AuthStack"
+  }
+);
+
+export default createAppContainer(switchNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ about-screen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AboutScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>I'm the third screen</Text>
+    </View>
+  );
+};
+export default AboutScreen;
