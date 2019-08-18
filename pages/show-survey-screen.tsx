48c19329d00ef7e77a7ca98309db7c476774bc38
cmmadnat <cmmadnat@gmail.com>
2019-08-18 16:34:24 +0700

    add show survey

--- pages/show-survey-screen.tsx
+++ pages/show-survey-screen.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import { WebView } from "react-native";
+import { getHeaderInset } from "../components/header-inset";
 
 export interface IAppShowSurveyScreenProps {}
 
 export default function AppShowSurveyScreen(props: IAppShowSurveyScreenProps) {
+  const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `;
+
   return (
     <WebView
       style={{ flex: 1 }}
+      {...getHeaderInset()}
+      scalesPageToFit={false}
+      injectedJavaScript={INJECTEDJAVASCRIPT}
+      scrollEnabled
       source={{ uri: "http://run.ict.mahidol.ac.th:443/survey.html?id=97" }}
     />
   );
